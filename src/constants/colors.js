6774c41aff3846584675e87be04e649c9358651b
Rafael Freitas <rafael.vasconcelos@outlook.com>
2020-02-24 23:02:03 -0300

    Criação de constantes e helpers básicos.

---
+++ src/constants/colors.js
@@ -0,0 +1,16 @@
+export const colors = {
+  primary: '#673AB7',
+  primaryDark: '#512DA8',
+  secondary: '#19BFB7',
+  success: '#085F16',
+  info: '#172760',
+  warning: '#FFE045',
+  danger: '#FF4081',
+  dark: '#252525',
+  grey: '#757575',
+  divider: '#BDBDBD',
+  background: '#F3F3F3',
+  white: '#FFFFFF',
+  black: '#000000',
+  transparent: 'transparent'
+};
---
+++ src/helpers/palette.js
@@ -0,0 +1,35 @@
+import { colors } from '../constants';
+
+const objColors = {
+  primary: { color: colors.primary, inverse: colors.white },
+  primaryDark: { color: colors.primaryDark, inverse: colors.white },
+  secondary: { color: colors.secondary, inverse: colors.dark },
+  success: { color: colors.success, inverse: colors.white },
+  warning: { color: colors.warning, inverse: colors.white },
+  danger: { color: colors.danger, inverse: colors.white },
+  dark: { color: colors.dark, inverse: colors.white },
+  grey: { color: colors.grey, inverse: colors.dark },
+  white: { color: colors.white, inverse: colors.dark }
+};
+
+export const palette = callBack => {
+  const list = {};
+  Object.keys(objColors).forEach(key => {
+    list[key] = callBack(objColors[key], key);
+  });
+  return list;
+};
+
+export const validState = [
+  'primary',
+  'primaryDark',
+  'secondary',
+  'tertiary',
+  'success',
+  'warning',
+  'danger',
+  'black',
+  'white',
+  'whitePrimary',
+  'grey'
+];

40a120b666f38b31cb0338ded01183e32439378a
Rafael Freitas <rafael.vasconcelos@outlook.com>
2020-03-02 02:43:39 -0300

    Integrar saldo na tela de home.

--- src/modules/home/components/Balance/index.js
+++ src/modules/home/components/Balance/index.js
@@ -1,19 +1,58 @@
-import React, {useState} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import React, {useCallback, useState} from 'react';
+import {View, Text, TouchableOpacity, ActivityIndicator} from 'react-native';
+import {showMessage} from 'react-native-flash-message';
+import {useFocusEffect, useNavigation} from '@react-navigation/native';
 import styles from './style';
+import {codesHttp, messagesHttpError, colors} from '~/constants';
+import {getBalance} from '~/services/transactions';
+import {formatAsCurrency} from '~/helpers';
 
 const Balance = () => {
-  const [stateBalance, setStateBalance] = useState('positive');
+  const [stateBalance, setStateBalance] = useState('neutral');
+  const [balance, setBalance] = useState(null);
+  const [loading, setLoading] = useState(false);
   const navigation = useNavigation();
+
+  useFocusEffect(
+    useCallback(() => {
+      getBalanceValue().finally(() => setLoading(false));
+    }, [])
+  );
+
+  const getBalanceValue = async () => {
+    setLoading(true);
+    try {
+      const {data} = await getBalance();
+      setBalance(data.balance);
+      setStateBalance(data.balance < 0 ? 'negative' : 'positive');
+    } catch (e) {
+      const {status, data} = e.response;
+
+      const msg =
+        status !== codesHttp.INTERNAL_SERVER_ERROR ? data.result : messagesHttpError.DEFAULT;
+
+      showMessage({
+        message: 'Atenção!',
+        description: msg,
+        type: 'warning'
+      });
+    }
+  };
 
   return (
     <View>
       <Text style={styles.label}>Confira sua carteira</Text>
-      <Text style={[styles.amountPrefix, styles[stateBalance]]}>
-        R$
-        <Text style={[styles.amountBalance, styles[stateBalance]]}> 20.230,00</Text>
-      </Text>
+      {!loading ? (
+        <Text style={[styles.amountPrefix, styles[stateBalance]]}>
+          R$
+          <Text style={[styles.amountBalance, styles[stateBalance]]}>
+            {' '}
+            {balance ? formatAsCurrency(balance) : '----'}
+          </Text>
+        </Text>
+      ) : (
+        <ActivityIndicator color={colors.primary} size="large" />
+      )}
       <TouchableOpacity onPress={() => navigation.navigate('ListTransaction')}>
         <Text style={styles.extract}>+ extrato detalhado</Text>
       </TouchableOpacity>
--- src/services/transactions.js
+++ src/services/transactions.js
@@ -4,6 +4,8 @@ const getTransactionsLimit = async limit => http().get(`/transactions/${limit}`)
 
 const getTransactionsDetails = async () => http().get(`/transactions/list/details`);
 
+const getBalance = async () => http().get(`/transactions/balance/details`);
+
 const setTransaction = async data => http().post(`/transactions`, {...data});
 
-export {setTransaction, getTransactionsLimit, getTransactionsDetails};
+export {setTransaction, getTransactionsLimit, getTransactionsDetails, getBalance};

ed765e2feb15f1490a9231469dd464066f12e116
Rafael Freitas <rafael.vasconcelos@outlook.com>
2020-03-02 01:38:03 -0300

    Adicionar comentário sobre médias e resumos mensais.

--- src/modules/home/components/AverageResume/index.js
+++ src/modules/home/components/AverageResume/index.js
@@ -13,19 +13,20 @@ const Average = () => {
 
   return (
     <View style={styles.averageResume}>
+      {/* Para não levar mais tempo na entrega do projeto as médias e o resumo mensal serão apenas de exemplo */}
       <View style={styles.average}>
-        <Item title="Média receita" value="12.230,00" />
+        <Item title="Média receita" value="2.230,00" />
         <Item title="Média gastos" state="negative" value="1.230,00" />
       </View>
       <Text style={styles.label}>Resumo mensal</Text>
       <View style={styles.average}>
-        <Item title="Atual" value="12.230,00">
+        <Item title="Janeiro" value="2.230,00">
           <Text style={[styles.label, styles.negative]}>R$ 1.234,00</Text>
         </Item>
-        <Item title="Anterior" value="12.230,00">
+        <Item title="Fevereiro" value="2.230,00">
           <Text style={[styles.label, styles.negative]}>R$ 1.234,00</Text>
         </Item>
-        <Item title="Há dois meses" value="12.230,00">
+        <Item title="Março" value="2.230,00">
           <Text style={[styles.label, styles.negative]}>R$ 1.234,00</Text>
         </Item>
       </View>
